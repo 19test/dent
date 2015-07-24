87b65acab2917912ca94a94da8cc8fb0e377bb51
Macpeters <peters.maryanne@gmail.com>
2015-07-24 17:05:21 -0400

    style practices

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -3,7 +3,7 @@ class Users::ProfilesController < ApplicationController
   before_filter :authenticate_user!, except: [:show]
 
   def show
-    @default_image = "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS1Vam2cBYE0BiCYXcAjB_xM92rIMQQ7YZ9rY9levRJ01eerIUYnPtwoQBl"
+    @default_image = default_image
     @user = User.find(params[:id])
     @professionals = ProfessionalsUsers.where("user_id =  ?", 1)
   end
